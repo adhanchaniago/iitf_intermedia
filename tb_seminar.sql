3cd9d156feb6f474f4f6da00e0b15bc864cc8707
Dwi Candra Permana <dwichan@outlook.com>
2019-09-14 04:29:44 +0700

    Update tabel baru dalam db

---
+++ tb_seminar.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Sep 2019 pada 23.28
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iitf_intermedia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_seminar`
+--
+
+CREATE TABLE `tb_seminar` (
+  `id_daf` varchar(7) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `notelp` varchar(13) NOT NULL,
+  `institusi` varchar(60) NOT NULL,
+  `status_bayar` tinyint(1) NOT NULL DEFAULT '0',
+  `status_ulang` tinyint(1) NOT NULL DEFAULT '0',
+  `tgl_daftar` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `htm_status` enum('presale','sale','ots') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_seminar`
+--
+ALTER TABLE `tb_seminar`
+  ADD PRIMARY KEY (`id_daf`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
